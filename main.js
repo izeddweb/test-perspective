ae3f8503e789ba6ef37aea3a8be8790d466b6f35
izeddweb <abzedd61@gmail.com>
2022-10-08 18:13:35 +0200

    first work in

---
+++ main.js
@@ -0,0 +1,6 @@
+// const bull5 = document.querySelector('.bull-5');
+// const bull1 = document.querySelector('.bull-1');
+// const rotate = function () {
+//   bull5.style.backgroundColor = 'red';
+// }
+// rotate();
